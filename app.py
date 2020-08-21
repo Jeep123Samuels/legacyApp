d6a56db9c8de76c41d97178b41fd39c2c33d93d7
Jean-Pierre Samuels <jeep123samuels@gmail.com>
2020-08-21 13:17:19 +0200

    chore(app): update settings files

--- app.py
+++ app.py
@@ -5,4 +5,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-    return "<h1>sdkvjbsdjkvdsfv</h1>"
+    return "<h1>Many Ways To Solve a Problem</h1>"
+
+
+@app.route("/melisa/samuels/")
+def love_view_2():
+    return "bly jy is myne my lief... mwah. JP"
+
+
+@app.route("/melisa/stuur/soen/vir/jp")
+def love_view_3():
+    return "Mwah op jou bek vrou lief"
+
+
+@app.route("/jp/maak/ogies/vir/melisa/")
+def love_view_4():
+    return "Ek gaan jou stoot vanaand... mwah"
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from app import app
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
